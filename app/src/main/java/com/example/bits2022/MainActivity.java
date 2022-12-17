ca41cb2f239273f9dbabdae517669b69123b9da7
marcel <marcel.calleja@estudiantat.upc.edu>
2022-12-17 22:43:14 +0100

    activity_main y main_activity acabados

--- app/src/main/java/com/example/bits2022/MainActivity.java
+++ app/src/main/java/com/example/bits2022/MainActivity.java
@@ -30,13 +30,13 @@ public class MainActivity extends AppCompatActivity {
         apuntsBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                changeApp();
             }
         });
         exitBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeApp();
+                finish();
             }
         });
 
